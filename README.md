0e21433e8d67772ae3c9d7bf5c29e8dec73c9b34
Phil Reed <Phil.Reed@manchester.ac.uk>
2017-07-17 14:02:41 +0100

    Unzipping largely works.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CSR_BB_Europe
 Analyse the CSR reports from Bloomberg on European companies.
+
+## Contents
+Various stages below.
+1. Unzip 
+2. (tba)
+
+Also 0. testing
+
+## Summary
+### 1. Unzip
+Unzip all reports so far. One folder per country (index). 
+All years and report types mixed up. 
+Can't use given year in filenames (this is upload date, not report date).
---
+++ step1_unzip.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python    
+
+# Unzip everything from "U:\Ser-Huang_Poon\CSR_Europe\*.zip"
+# to "U:\Phil_Read\CSR_Europe\unzipped_raw\"
+
+import utilCSR
+
+zipPath = "U:/Ser-Huang_Poon/CSR_Europe/"
+unzipPath = "U:/Phil_Read/CSR_Europe/unzipped_raw/"
+
+from os import listdir
+from os.path import isfile, join
+zipfiles = [f for f in listdir(zipPath) if (isfile(join(zipPath, f)) )]
+
+import zipfile
+import os.path
+for zip in zipfiles:
+    extractFolder = utilCSR.getIsoFromString(zip)
+    extractPath = os.path.abspath(os.path.join(unzipPath, extractFolder))
+    try:
+        zipfile.ZipFile(zipPath + zip).extractall(extractPath)
+    except zipfile.BadZipfile:
+        import sys
+        print ('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+        print('File:' + zip)
+        raise e
