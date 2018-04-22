33527cf0bf3a73f7f69084e67e87d5ab6ae43a5d
PhilReedData <Phil.Reed@manchester.ac.uk>
2018-04-22 19:02:47 +0100

    Step 1 Europe implemented, part-run.

---
+++ v2018/step11_perform_unzip.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python    
+
+# Unzip everything to one folder
+
+### Change this True/False to run Europe or UK version of code. ###
+isEurope = True # UK not implemented yet
+
+# Import my utilities
+import utilCSR
+
+# Paths
+zipPathEurope = "U:/Ser-Huang_Poon/Europe_CSRzipBloomberg/"
+unzipPathEurope = "U:/Phil_Read/CSR_Europe/unzipped_allinone/"
+zipPathUK = "" # there are three folders of zips! Rethink this, loop or similar
+unzipPathUK = "U:/Phil_Read/CSR_UK/unzipped_allinone/"
+zipPath = zipPathEurope if isEurope else zipPathUK
+unzipPath = unzipPathEurope if isEurope else unzipPathUK
+tableOutPath = 'stats11EU.csv' if isEurope else 'stats11UK.csv'
+refinedTableOutPath = 'stats13EU.csv' if isEurope else 'stats13UK.csv'
+SEP = ','
+
+# Prepare dataframe and output table
+tableOut = open(tableOutPath,'w')
+columnHeadings = ['filenamefull', 'filetype', 'filenameshort','reporttype','uploadyear','ticker','country','unzip']
+tableOut.write(SEP.join(columnHeadings) + '\n')
+import pandas as pd
+df = pd.DataFrame(columns=columnHeadings)
+rowCount = 0
+
+# Get list of zip files
+from os import listdir
+from os.path import isfile, join
+zipfiles = [f for f in listdir(zipPath) if (isfile(join(zipPath, f)) )]
+zipfiles.sort()
+print(len(zipfiles),'zipfiles to unzip')
+
+# Loop zip files
+import zipfile
+import os.path
+for zip in zipfiles:
+    countryCode = utilCSR.getIsoFromString(zip) if isEurope else "GB"
+    extractPath = os.path.abspath(unzipPath)
+    try:
+        zipobject = zipfile.ZipFile(zipPath + zip)
+        namelist = zipobject.namelist()
+        print(len(namelist),'files to extract within', countryCode, zip)
+        for name in namelist :
+            extractAlreadyExists = os.path.isfile(name)
+            
+            # For each file within the zip, add a row to tableOut
+            filenamefull = name
+            filetype = utilCSR.rawFilename2Ext(name)
+            filenameshort = utilCSR.rawFilename2Short(name)
+            reporttype = utilCSR.rawFilename2ReportType(name)
+            uploadyear = str(utilCSR.rawFilename2UploadYear(name))
+            ticker = utilCSR.rawFilename2Ticker(name)
+            country = countryCode
+            unzip = '0' if extractAlreadyExists else '1'
+            rowValues = [filenamefull,filetype,filenameshort,reporttype,uploadyear,ticker,country,unzip]
+            tableOut.write(SEP.join(rowValues) + '\n')
+            
+            # if df contains name, update country column, else add new row
+            match = df.loc[df['filenamefull'] == name]
+            if len(match) > 0:
+                previousCountry = match['country']
+                if previousCountry != country:
+                    df.loc[df['filenamefull'] == name, 'country'] = previousCountry + "|" + country
+            else:
+                df.loc[rowCount] = rowValues
+                rowCount += 1
+            
+            if extractAlreadyExists:
+                print('Skipping already unzipped', name, country)
+            else:
+                # Do the extraction for this report
+                zipobject.extract(name, extractPath)
+                print('Unzipped', name, country)
+            
+        ## Do the extraction for this whole zip
+        ##zipobject.extractall(extractPath)
+        print('Finished extracting', zip)
+    except zipfile.BadZipfile as e:
+        import sys
+        print ('Error on line {}'.format(sys.exc_info()[-1].tb_lineno))
+        print('File:' + zip)
+        raise e
+        
+# Write out tables
+tableOut.close()
+# Drop unzip column 
+df = df.drop('unzip',1)
+# Rename now last column of df; country to countries
+newColumnHeadings = columnHeadings[:-2] + ['countries']
+df.rename(index=str, columns = newColumnHeadings, inplace = True)
+# Export dataframe
+df.to_csv(refinedTableOutPath, encoding='utf-8', index=False)
---
+++ v2018/utilCSR.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python 
+
+# Utils
+
+# Dict of Bloomberg index code to ISO country code.
+INDEX2ISO = {
+    'ATX'       :'AT', 
+    'BEL'       :'BE', 
+    'OMXC'      :'DK', 
+    'OMXH'      :'FI', 
+    'CAC'       :'FR', 
+    'DAX'       :'DE', 
+    'ISEQ'      :'IE', 
+    'FTSEMIBN'  :'IT', 
+    'AEX'       :'NL', 
+    'OBX'       :'NO', 
+    'PSI'       :'PT', 
+    'IBEX'      :'ES', 
+    'OMX2'      :'SE', 
+    'SMI'       :'CH', 
+    'UKX'       :'GB' 
+}
+
+# Dict of codes for the known report types.
+# Note: there are other AR strings! Preliminary_Annual, 20-F
+REPORT_TYPES = {
+    'CR'    :'Corporate_Responsibility',
+    'AR'    :'Annual_Report',
+    'ESG'   :'ESG_Releases'
+}
+
+# Given a zip filename that begins with a Bloomberg index code,
+# return ISO country code.
+def getIsoFromString(instring):
+    if instring in INDEX2ISO:
+        return INDEX2ISO[instring]
+    for index in INDEX2ISO.keys():
+        # if instring begins with this index, return its iso
+        if instring[:len(index)] == index:
+            return INDEX2ISO[index]
+    print(instring)
+    return 'ZZ_OTHER/' + instring[:-4]
+    
+def test_getIsoFromString():
+    test1 = getIsoFromString("OMXH25")
+    print(test1, " (should be FI)")
+    test2 = getIsoFromString("IBEX2012_BB_Docs_071417_110458.zip")
+    print(test2, " (should be ES)")
+    
+# Given a raw Bloomberg report filename, return the company ticker
+# Ticker is between first and second underscore. 
+def rawFilename2Ticker(rawFilename):
+    firstU = rawFilename.index('_')
+    secondU = rawFilename.index('_', firstU + 1)
+    return rawFilename[firstU+1:secondU]
+
+def test_rawFilename2Ticker():
+    ticker = rawFilename2Ticker("012011_MRK_Corporate_Responsibility_US000000002016945409.pdf")
+    print(ticker, 'MRK')
+    
+# Given a raw Bloomberg report filename,
+# return a code for the report type.
+def rawFilename2ReportType(rawFilename):
+    if "_Corporate_Responsibility_" in rawFilename:
+        return "CR"
+    elif "_Annual_Report_" in rawFilename or "_Preliminary_Annual_" in rawFilename or "_20-F_" in rawFilename:
+        return "AR"
+    elif "_ESG_Releases_" in rawFilename:
+        return "ESG"
+    return "UNKNOWN"
+
+# Given a raw Bloomberg report filename,
+# return a 4-digit year the report was uploaded
+def rawFilename2UploadYear(rawFilename):
+    # Filename begins MMDDYY.
+    try: 
+        uploadYear = int("20" + rawFilename[4:6])
+    except ValueError:
+        uploadYear = -1
+    return uploadYear
+
+# Given a raw Bloomberg report filename,
+# return the file extension, or blank if none
+def rawFilename2Ext(rawFilename):
+    # Get characters after dot
+    try:
+        dot = rawFilename.rindex('.')
+    except ValueError:
+        return ''
+    return rawFilename[dot+1:]
+    
+# Given a raw Bloomberg report filename,
+# return the filename without extension, or whole if no extension
+def rawFilename2Short(rawFilename):
+    # Get characters after dot
+    try:
+        dot = rawFilename.rindex('.')
+    except ValueError:
+        return rawFilename
+    return rawFilename[:dot]
+    
+if __name__ == "__main__":
+    #test_getIsoFromString()
+    #test_rawFilename2Ticker()
+    
+    zipPath = "U:/Ser-Huang_Poon/CSR_Europe/"
+    unzipPath = "U:/Phil_Read/CSR_Europe/unzipped_raw/"
+    
+    from os import listdir
+    from os.path import isfile, join
+    zipfiles = [f for f in listdir(zipPath) if (isfile(join(zipPath, f)) )]
+    print(len(zipfiles))
+    
