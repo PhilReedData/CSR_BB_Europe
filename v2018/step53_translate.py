a46e3ce2c903320624d09de2649f997dfb12a4f7
PhilReedData <Phil.Reed@manchester.ac.uk>
2018-05-14 08:51:20 +0100

    Translate working. Two bits untested.

--- v2018/step53_translate.py
+++ v2018/step53_translate.py
@@ -3,7 +3,7 @@
 # translate non-English to English, add column to stats table, matching filename
 
 ### Change this True/False to run Europe or UK version of code. ###
-isEurope = False
+isEurope = True
 
 statsPathIn  = 'stats51EU.csv' if isEurope else 'stats51UK.csv'
 statsPathOut = 'stats53EU.csv' if isEurope else 'stats53UK.csv'
@@ -24,23 +24,69 @@ df.fillna('',inplace=True) # replace any blank (nan) with empty string, eg filen
 log = open(logPath,'w')
 log.write('filenamefull,filenametxttrans\n')
 
+from os.path import isfile, join, exists, getsize
+from shutil import copy2
+import utilTranslate
 
 #Function callTranslate(filenamefull, filenametxt, lang)
 #	If filenametxt exists and lang <> en, Call utility
 def callTranslate(filenamefull, filenametxt, lang):
     filenametxttrans = ''
-    if len(filenametxt) > 0 and lang != 'en' and lang != '-1':
-        ...do translate # maybe skip tiny files too
+    if len(filenametxt) > 0:
+        # Skip if file already translated/copied # NOT YET TESTED
+        filenametxttransExisting = filenametxt[:-4] + '.tran.txt'
+        if isfile(join(txtTransPath, filenametxttransExisting)):
+            return filenametxttransExisting
+        filenametxttransExisting = filenametxt[:-4] + '.orig.txt'
+        if isfile(join(txtTransPath, filenametxttransExisting))
+            return filenametxttransExisting
+            
+        size = getsize(join(txtPath,filenametxt))
+        MIN_SIZE = 64 # bytes
+        if len(lang) > 1 and lang != 'en' and lang != '-1' and size>MIN_SIZE:
+            # do translate 
+            print ('Translating ' + txtPath + filenametxt)
+            foreignText = utilTranslate.loadFile(join(txtPath, filenametxt))
+            try:
+                resultText = utilTranslate.translateInChunks(foreignText)
+                charsOut = str(len(resultText))
+                print ('Writing ' + charsOut + ' character(s).')
+                # Replace the file extension from .txt to .tran.txt
+                filenametxttrans = filenametxt[:-4] + '.tran.txt'
+                utilTranslate.writeFile(resultText, join(txtTransPath,filenametxttrans))
+            except ValueError:
+                import sys
+                filenametxttrans = '_ERROR ON LINE ' + str(sys.exc_info()[-1].tb_lineno)
+                print ('Error translating. Skipping.')
+        elif lang == 'en' and size>MIN_SIZE:
+            # Copy the file to .orig.txt
+            print ('Copying already English file ' + filenametxt)
+            filenametxttrans = filenametxt[:-4] + '.orig.txt'
+            copy2(join(txtPath,filenametxt), join(txtTransPath,filenametxttrans))
+        else:
+            print ('Not copying ' + filenametxt)
+    log.write(filenamefull + ',' + filenametxttrans + '\n')    
     return filenametxttrans
 
-#Apply function getLang to df, save new column filenametxttrans.
+#Apply function callTranslate to df, save new column filenametxttrans.
 df['filenametxttrans'] = df.apply(
-    lambda x: getLang(x['filenamefull'], x['filenametxt'], x['lang']), axis=1
+    lambda x: callTranslate(x['filenamefull'], x['filenametxt'], x['lang']), axis=1
 )
 
-
+# Add summary column to easily see what was translated or not # NOT TESTED
+def summarizeTranslate(filenametxttrans):
+    summary = ""
+    if filenametxttrans.endswith('.tran.txt'):
+        summary = "Translated to English txt"
+    elif filenametxttrans.endswith('.orig.txt'):
+        summary = "Already English txt"
+    elif filenametxttrans.startswith('_ERROR'):
+        summary = "Error translating txt"
+    else:
+        summary = "No txt to translate"
+    return summary
+df['translated'] = df.apply(lambda x: summarizeTranslate(x['filenametxttrans']), axis=1)
+    
 #Save df to stats53.csv.
 df.to_csv(statsPathOut, index=False, encoding='utf-8')
 log.close()
-
-
---
+++ v2018/utilTranslate.py
@@ -0,0 +1,160 @@
+﻿#!/usr/bin/python    
+# -*- coding: utf-8 -*-
+
+# utilTranslate
+# Translate text to English
+# 2017-09-19
+
+# Make Python 2 behave like Python 3 for / and // division:
+from __future__ import division
+
+import codecs 
+import time
+from os import makedirs
+from os.path import exists
+from math import ceil
+
+
+
+# test input, in Swedish
+input = u'''
+Några ord från koncernchefen
+Hållbar utveckling
+Våra policies och åtaganden
+Hälsa och säkerhet
+Anställdas utveckling och delaktighet
+Balans mellan arbete och fritid
+Djurförsök och etik
+FoU-investeringar i Bangalore, Indien
+Miljöprestanda Jämförelse av resultat Användning av naturresurser Avfall Användning av vatten Utsläpp till omgivningen Vatten Luft Energi- och klimatfrågan Transporter Hur vi uppfyller myndigheternas krav Läkemedel i miljön Andra frågor
+Koncernchefens pris för framstående prestationer
+Policy och etiska regler
+Kontakta oss
+'''
+
+fileInLarge = "U:/Phil_Read/CSR_UK_latest_txt_all_flat/2012_15_GB00BYSRJ698_CR1.txt"
+fileInSmall = "U:/Phil_Read/CSR_UK_latest_txt_all_flat/2009_20_GB0005758098_CR1.txt"
+fileInSV = "U:/Phil_Read/CSR_UK_latest_txt_all_flat/2000_35_GB0009895292_CR1.txt"
+fileInSVMedium = "U:/Phil_Read/CSR_UK_latest_txt_all_flat/2000_35_GB0009895292_CR1.txt"
+
+#METHOD 1: py-translate 1.0.3 (older but may be more reliable)
+# https://pypi.python.org/pypi/py-translate
+# pip install py-translate
+# pip install futures
+#from translate import translator
+#result = translator('sv', 'en', input)
+#print (result)
+# WE GOT 503 ERRORS (BLOCKED) AFTER 3 RUNS!
+
+#METHOD 2: googletrans 2.1.4 (newer)
+# https://pypi.python.org/pypi/googletrans
+# pip install googletrans
+# LATEST: translation document limit is 15k characters. Ours are larger. 
+#   Need to break them up, sensibly.
+from googletrans import Translator
+translator = Translator()
+
+#METHOD 3: translate
+# pip install translate
+# https://github.com/terryyin/google-translate-python
+# Uses http://mymemory.translated.net/  and 1000 words per day
+# Default en to cn, change: auto to en.
+
+# METHOD 2
+def translateToEn(input):
+    chars = len(input)
+    charsS = str(chars)
+    if chars < 4:
+        print ('Not translating short string of ' + charsS + ' chars')
+        return u''
+    print('Pausing 1 second before translating ' + charsS +' chars')
+    time.sleep(1)
+    result = translator.translate(input)
+    return result.text
+
+# Take the whole report (string) and return the whole translation. 
+# For now, crudely break every 5000 characters.
+def translateInChunks(input):
+    chunkSize = 5000
+    charsTranslated = 0
+    charsTotal = len(input)
+    charsS = str(charsTotal)
+    chunksTranslated = 0
+    numChunks = int(ceil(charsTotal / chunkSize))
+    resultSoFar = u''
+    print('Input has ' + charsS + ' character(s), break into ' + str(numChunks)+ ' chunk(s)')
+    
+    while (charsTranslated < charsTotal):
+        start = charsTranslated
+        end = start + chunkSize
+        
+        # Choose a clean break point
+        lastLineBreak = input.rfind('\n', start, end)
+        if lastLineBreak > start:
+            print ('  Adjusting end from ' + str(end) + ' to ' + str(lastLineBreak))
+            end = lastLineBreak
+        else:
+            print ('  No linebreak found in chunk. ' + str(lastLineBreak))
+        
+        if end > charsTotal:
+            end = charsTotal
+        #print ('chunksTranslated', chunksTranslated, 'charsTranslated (start)', charsTranslated, 'end', end)
+        resultSoFar  += translateToEn(input[start:end])
+        chunksTranslated += 1
+        charsTranslated += (end - start)
+    
+    return resultSoFar
+
+def loadFile(fileIn=fileInLarge):
+    with codecs.open(fileIn, 'r', encoding='utf8', errors='ignore') as fileIn:
+        foreignText = fileIn.read()
+    return foreignText
+    # Do translation, don't overload server
+    #chars = str(len(foreignText))
+
+def writeFile(englishText, path):
+    with codecs.open(path, 'w', encoding='utf8', errors='ignore') as fileOut:
+        fileOut.write(englishText)
+
+def test01():
+    result = translator.translate(input)
+    #foreignText = loadFile(fileInSV)
+    #resultText = translateInChunks(foreignText)
+    #result = translator.translate(foreignText)
+    #resultSrc = result.src
+    #resultDest = result.dest # = 'en'
+    resultText = result.text
+    #resultPronunciation = result.pronunciation # = None
+    #print ('Translated from '+ resultSrc + ' to ' + resultDest + ':')
+    print (resultText[:200] + '...')
+    quit()
+    
+# Read in list to translate, save back
+def test02():
+    fromDir = "U:/Phil_Read/CSR_UK_latest_txt_all_flat/"
+    toDir = "U:/Phil_Read/CSR_UK_latest_txt_all_flat_trns/"
+    listPath = "./x_toBeTranslated.txt"
+    if not exists(toDir):
+        makedirs(toDir)
+    foreignReports = open(listPath,'r').read().split('\n')
+    for filename in foreignReports: 
+        print ('Read from ' + fromDir + filename)
+        foreignText = loadFile(fromDir + filename) # should use os join
+        resultText = translateInChunks(foreignText)
+        charsOut = str(len(resultText))
+        print ('Writing ' + charsOut + ' character(s).')
+        writeFile(resultText, toDir+filename)
+
+# Read in a chunkable foreign report
+def test03():
+    fromDir = "U:/Phil_Read/CSR_UK_latest_txt_all_flat/"
+    toDir = "U:/Phil_Read/CSR_UK_latest_txt_all_flat_trns/"
+    print ('Read from ' + fileInSVMedium)
+    foreignText = loadFile(fileInSVMedium)
+    resultText = translateInChunks(foreignText)
+    charsOut = str(len(resultText))
+    print ('Writing ' + charsOut + ' character(s).')
+    writeFile(resultText, 'testSV.txt')
+    
+if __name__ == "__main__":
+    test03()
